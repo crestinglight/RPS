8665b51eceb3b7fd6e00b231bb7db60afa8e0979
Evin Grost <evin.grost@gmail.com>
2017-01-30 22:36:33 -0600

    Working with first automated test.

---
+++ automated-tests.js
@@ -0,0 +1,1 @@
+require_relative "rps.js"
--- rps.js
+++ rps.js
@@ -1,15 +1,19 @@
 var prompt = require('/Users/evingrost/Code/Rock-Paper-Scissors/node_modules/prompt');
 
+//Starts the prompting library.
 prompt.start();
 
+//Begins the function that runs the program, starting the game.
 startGame();
 
+//Start game function. Starts by telling player 1 to choose a weapon, then moves on to get their input.
 function startGame(){
 
 	console.log("Player 1, choose your weapon!");
 	getPlayer1();
 }
 
+//Gets player 1's input. Checks the validity of their answer. If the answer is valid, moves on to player 2. If not, tells them their options and restarts the function.
 function getPlayer1(){
 
 	prompt.get(['P1Weapon'], function (err, result) {
@@ -27,6 +31,7 @@ function getPlayer1(){
 	});
 }
 
+//Gets player 2's input. Calls to check the validity of the answer. If the answer is valid, moves on to decide who won. If not, tells them their options and restarts the function.
 function getPlayer2(p1weaponChoice){
 
 	console.log("Player 2, choose your weapon!");
@@ -44,19 +49,27 @@ function getPlayer2(p1weaponChoice){
 	});
 }
 
+//Function which looks at the choices the players made and determines a winner. If they chose the same, they tied!
 function whoWon(p1weapon, p2weapon){
 
 	if (p1weapon == p2weapon){
-		console.log("It's a tie!");
+		var winnerResult = "It's a tie!"
+		console.log(winnerResult);
+		return winnerResult;
 	}	
 	if ((p1weapon == "rock" && p2weapon == "scissors") || (p1weapon == "scissors" && p2weapon == "paper") || (p1weapon == "paper" && p2weapon == "rock")){
-		console.log("Player 1 wins!!!");
+		var winnerResult = "Player 1 wins!!!"
+		console.log(winnerResult);
+		return winnerResult;
 	}
 	if ((p2weapon == "rock" && p1weapon == "scissors") || (p2weapon == "scissors" && p1weapon == "paper") || (p2weapon == "paper" && p1weapon == "rock")){
-		console.log("Player 2 wins!!!");
+		var winnerResult = "Player 2 wins!!!"
+		console.log(winnerResult);
+		return winnerResult;
 	}
 }
 
+//Checks the validity of the players' answers. If they didn't type in rock, paper, or scissors, returns an invalid answer.
 function checkValidity(weapon){
 
 	if (weapon == "rock" || weapon == "paper" || weapon == "scissors"){
@@ -66,6 +79,25 @@ function checkValidity(weapon){
 		return false;
 	}
 }
+
+// testWinner();
+// function testWinner(){
+
+// 	var actual = whoWon("rock", "scissors");
+// 	var expected = "Player 1 wins!!!"
+// 	if (actual == expected){
+// 		console.log("Pass.");
+// 	}
+// 	else{
+// 		console.log("Fail. I expected " + expected + ", I got " + actual);
+// 	}
+// }
+
+
+
+
+
+
 
 
 
