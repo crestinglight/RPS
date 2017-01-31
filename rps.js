af899c2a9230a6304e528ec2eca915b2c6bd04ef
Evin Grost <evin.grost@gmail.com>
2017-01-30 20:42:04 -0600

    Player 1 weapon input checks for validity. Working great so far.

--- rps.js
+++ rps.js
@@ -1,15 +1,8 @@
 var prompt = require('/Users/evingrost/Code/Rock-Paper-Scissors/node_modules/prompt');
 
-startGame;
-
 prompt.start();
 
-prompt.get(['P1Weapon'], function (err, result) {
-
-	
-	
-	
-});
+startGame();
 
 function startGame(){
 
@@ -20,22 +13,45 @@ function startGame(){
 function getPlayer1(){
 
 	prompt.get(['P1Weapon'], function (err, result) {
-		console.log('Player 1, you chose ' + result.P1Weapon + '!');
+		
+		var lowerP1Weapon = result.P1Weapon.toLowerCase();
+		var isAnswerValid = checkValidity(lowerP1Weapon);
+		if (isAnswerValid == true){
+			console.log('Player 1, you chose ' + result.P1Weapon + '!');
+			getPlayer2(lowerP1Weapon);
+		}
+		else {
+			console.log("Your options are rock, paper, or scissors. Try again slugger.")
+			getPlayer1();
+		}
 	});
-	getPlayer2();
 }
 
-function getPlayer2(){
+function getPlayer2(p1weaponChoice){
 
 	console.log("Player 2, choose your weapon!");
 	prompt.get(['P2Weapon'], function (err, result) {
 
 	console.log('Player 2, you chose ' + result.P2Weapon + '!');
-	});
+	checkValidity();
 	whoWon();
+	});
 }
 
 function whoWon(){
 
 	console.log("You're all winners here!")
 }
+
+function checkValidity(weapon){
+
+	if (weapon == "rock" || weapon == "paper" || weapon == "scissors"){
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
+
