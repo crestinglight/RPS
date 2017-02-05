1dd670679c8a588a59c9dc21162a4c569b9de1c8
Evin Grost <evin.grost@gmail.com>
2017-02-05 16:23:33 -0600

    Web application working, but ugly.

--- controller.rb
+++ controller.rb
@@ -1,3 +1,36 @@
+require_relative 'rpsWeb.rb'
 require 'sinatra'
+require 'pry'
 
-YOUR CONTROLLERS GO HERE ("GET" AND "POST"). LOOK AT SIN.RB FOR REFERENCE.
+get("/"){
+	erb :homepage
+}
+
+post("/"){
+	p1Choice = params["weapon1"]
+	p2Choice = params["weapon2"]
+	winner = whoWon(p1Choice, p2Choice)
+	if ($player1Score == 3) || ($player2Score == 3)
+		redirect("/winner")
+	else
+	redirect("/roundwin?roundwinner=#{winner}")
+	end
+	erb :homepage
+}
+
+get("/roundwin"){
+	@printwinner = params.values[0]
+	@currentP1Score = $player1Score.to_s
+	@currentP2Score = $player2Score.to_s
+
+	erb :roundwin
+}
+
+post("/roundwin"){
+	erb :roundwin
+}
+
+get("/winner"){
+	erb :winner
+}
+
---
+++ rpsWeb.rb
@@ -0,0 +1,121 @@
+$player1Score = 0
+$player2Score = 0
+
+#Start game def. Starts by telling player 1 to choose a weapon, then moves on to get their input.
+# def startGame
+# 	puts "Player 1, choose your weapon!"
+# 	getPlayer1
+# end
+
+#Gets player 1's input. Checks the validity of their answer. If the answer is valid, moves on to player 2. If not, tells them their options and restarts the def.
+# def getPlayer1(p1choice)
+
+# 	p1weapon = gets.chomp
+		
+# 	lowerP1Weapon = p1weapon.downcase
+# 	isAnswerValid = checkValidity(lowerP1Weapon)
+# 	if isAnswerValid === true
+# 		puts "Player 1, you chose " + lowerP1Weapon + "."
+# 		getPlayer2(lowerP1Weapon)
+
+# 	else 
+# 		puts "Wrong answer, try again."
+# 		startGame
+# 	end
+# end
+
+# #Gets player 2's input. Calls to check the validity of the answer. If the answer is valid, moves on to decide who won. If not, tells them their options and restarts the def.
+# def getPlayer2(p1choice)
+
+# 	puts "Player 2, choose your weapon!"
+# 	p2weapon = gets.chomp
+# 	lowerP2Weapon = p2weapon.downcase
+# 	isAnswerValid = checkValidity(lowerP2Weapon)
+# 	if isAnswerValid === true
+# 		puts "Player 2, you chose " + lowerP2Weapon + "."
+# 		whoWon(p1weaponChoice, lowerP2Weapon)
+
+# 	else
+# 		puts "Wrong answer, try again."
+# 		getPlayer2(p1weaponChoice)
+# 	end
+# end
+
+#def which looks at the choices the players made and determines a winner. If they chose the same, they tied!
+def whoWon(p1weapon, p2weapon)
+
+	if (p1weapon === p2weapon) && (p1weapon != nil) && (p2weapon != nil)
+		winnerResult = "It's a tie!"
+		#startGame
+		return winnerResult
+	end	
+
+	if (p1weapon === "rock" && p2weapon === "scissors") || (p1weapon === "scissors" && p2weapon === "paper") || (p1weapon === "paper" && p2weapon === "rock")
+		winnerResult = "Player 1 wins!!!"
+		addScore(winnerResult)
+		return winnerResult
+	end
+
+	if (p2weapon === "rock" && p1weapon === "scissors") || (p2weapon === "scissors" && p1weapon === "paper") || (p2weapon === "paper" && p1weapon === "rock")
+		winnerResult = "Player 2 wins!!!"
+		addScore(winnerResult)
+		return winnerResult
+	end
+end
+
+# #Checks the validity of the players' answers. If they didn't type in rock, paper, or scissors, returns an invalid answer.
+# def checkValidity(weapon)
+# 	lowerWeapon = weapon.downcase
+
+# 	if lowerWeapon === "rock" || lowerWeapon === "paper" || lowerWeapon === "scissors"
+# 		return true
+
+# 	else
+# 		return false
+# 	end
+# end
+
+def addScore(winResult)
+
+	if winResult == "Player 1 wins!!!"
+		$player1Score = $player1Score + 1
+		# keepScore
+
+	end
+
+	if winResult == "Player 2 wins!!!"
+		$player2Score = $player2Score + 1
+		# keepScore
+	end
+end
+
+# def p1Wins
+# 	p2score = $player2Score.to_s
+# 	$player1Score = 0
+# 	$player2Score = 0
+# end
+
+# def p2Wins
+# 	p1score = $player1Score.to_s
+# 	#p2score = $player2Score.to_s
+# 	$player1Score = 0
+# 	$player2Score = 0
+# end
+
+def keepScore
+	
+	if $player1Score < 3 && $player2Score < 3
+		#startGame
+
+	# elsif $player1Score === 5 && $player2Score < 5
+	# 	p1Wins
+
+	# elsif $player1Score < 5 && $player2Score === 5
+	# 	p2Wins
+	end
+end
+
+
+
+#Begins the def that runs the program, starting the game.
+#startGame
